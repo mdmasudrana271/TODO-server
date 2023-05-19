f956f0440bd104c3d348dd85b7a8d5d70cdea71a
Md Abdullah Al Masud <126868744+abdullah-almasud@users.noreply.github.com>
2023-05-19 09:05:18 +0600

    create todo details route

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 const express  = require('express');
-const cors = require('cors');
-const jwt = require('jsonwebtoken');
-const { MongoClient, ServerApiVersion } = require('mongodb');
-require('dotenv').config()
+const cors = require('cors')
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
+require('dotenv').config()
 
 const app = express();
 
 
 // midleware 
 
-app.use(cors())
-app.use(express());
+app.use(cors());
+app.use(express.json());
 
 
 
@@ -20,25 +19,19 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@clu
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
-function verifyJWT(req, res, next) {
-    const authHeader = req.headers.authorization;
-    if (!authHeader) {
-      return res.status(401).send("unauthorized access");
-    }
-    const token = authHeader.split(" ")[1];
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
-      if (err) {
-        return res.status(403).send({ message: "forbidden access" });
-      }
-      req.decoded = decoded;
-      next();
-    });
-  }
-
 
 async function run(){
     try {
         const todosCollection = client.db('todo-app').collection('todos')
+        const usersCollection = client.db('todo-app').collection('users')
+
+
+        app.post("/users", async (req, res) => {
+          const user = req.body;
+          const result = usersCollection.insertOne(user);
+          res.send(result);
+          console.log(user)
+        });
 
         app.post('/add-todo', async(req, res)=>{
           const todo = req.body;
@@ -47,16 +40,31 @@ async function run(){
         })
 
 
+        app.get('/all-todo', async(req, res)=>{
+          const query = {};
+          const result = await todosCollection.find(query).toArray();
+          res.send(result);
+        })
+
+        app.get("/details/:id", async(req, res)=>{
+          const id = req.params.id;
+          const query = { _id: new ObjectId(id) };
+          const result = await todosCollection.findOne(query);
+          res.send(result)
+          console.log(result)
+        })
+        
+
+
     }
     finally{
 
     }
 }
 
-run().catch(error=>{
-    console.log(error)
-})
-
+run().catch((error) => {
+  console.log(error.message);
+});
 
 
 app.get('/', (req, res)=>{
