420186a354994d8a2ea7cb382ddd86d6cbd67210
Md Abdullah Al Masud <126868744+abdullah-almasud@users.noreply.github.com>
2023-05-30 13:43:20 +0600

    todo server completed

--- index.js
+++ index.js
@@ -24,13 +24,13 @@ async function run(){
     try {
         const todosCollection = client.db('todo-app').collection('todos')
         const usersCollection = client.db('todo-app').collection('users')
+        const deletedCollection = client.db('todo-app').collection('deleted-todos')
 
 
         app.post("/users", async (req, res) => {
           const user = req.body;
           const result = usersCollection.insertOne(user);
           res.send(result);
-          console.log(user)
         });
 
         app.post('/add-todo', async(req, res)=>{
@@ -41,8 +41,9 @@ async function run(){
 
 
         app.get('/all-todo', async(req, res)=>{
-          const query = {};
-          const result = await todosCollection.find(query).toArray();
+          const email = req.query.email;
+          const query = { email: email };
+          const result = await todosCollection.find(query).sort({ $natural: -1 }).toArray();
           res.send(result);
         })
 
@@ -51,7 +52,70 @@ async function run(){
           const query = { _id: new ObjectId(id) };
           const result = await todosCollection.findOne(query);
           res.send(result)
-          console.log(result)
+        })
+
+        app.get("/trash", async(req, res)=>{
+          const email = req.query.email;
+          const query = {email: email};
+          const result = await deletedCollection.find(query).sort({ $natural: -1 }).toArray();
+          res.send(result)
+        })
+
+        app.patch("/edit", async(req, res)=>{
+          const {id,description} = req.body;
+          const filter = { _id: new ObjectId(id) };
+          const options = { upsert: true };
+          const updatedDoc = {
+            $set: {
+             description: description,
+            },
+          };
+          const result = await todosCollection.updateOne(
+            filter,
+            updatedDoc,
+            options
+          );
+          res.send(result);
+        })
+
+        app.patch('/update-status', async(req, res)=>{
+          const id = req.query.id;
+          const filter = { _id: new ObjectId(id) };
+          const options = { upsert: true };
+          const updatedDoc = {
+            $set: {
+             status: 'completed',
+            },
+          };
+          const result = await todosCollection.updateOne(
+            filter,
+            updatedDoc,
+            options
+          );
+          res.send(result);
+          
+        })
+
+        app.delete("/delete/:id", async(req, res)=>{
+          const id = req.params.id;
+          console.log(id)
+          const query = { _id: new ObjectId(id) };
+          const todo = await todosCollection.findOne(query);
+          const deleteCollection = await deletedCollection.insertOne(todo);
+          const filter = { _id: new ObjectId(id) };
+          const result = await todosCollection.deleteOne(filter);
+          res.send(result);
+          // res.send(addDeleteCollection)
+        })
+
+        app.delete('/undo-todo/:id', async(req, res)=>{
+          const id = req.params.id;
+          const query = { _id: new ObjectId(id) };
+          const todo = await deletedCollection.findOne(query);
+          const addTodoCollection = await todosCollection.insertOne(todo)
+          const filter = { _id: new ObjectId(id) };
+          const result = await deletedCollection.deleteOne(filter);
+          res.send(result)
         })
         
 
