c63186db2c2c7a461aa7ca76bc650f8d8dcf94e1
Md Abdullah Al Masud <126868744+abdullah-almasud@users.noreply.github.com>
2023-05-30 19:52:40 +0600

    project complete

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express  = require('express');
 const cors = require('cors')
+const jwt = require("jsonwebtoken");
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 require('dotenv').config()
@@ -19,12 +20,42 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@clu
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send("unauthorized access");
+  }
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({ message: "forbidden access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+}
+
+
 
 async function run(){
     try {
         const todosCollection = client.db('todo-app').collection('todos')
         const usersCollection = client.db('todo-app').collection('users')
         const deletedCollection = client.db('todo-app').collection('deleted-todos')
+
+
+
+        app.get("/jwt", async (req, res) => {
+          const email = req.query.email;
+          // const query = { email: email };
+          // const user = await usersCollection.findOne(query);
+          // if (user) {
+            
+          // }
+          // res.status(403).send({ accessToken: "" });
+          const token = jwt.sign({ email }, process.env.ACCESS_TOKEN_SECRET, {expiresIn: "1d",});
+          return res.send({ accessToken: token });
+        });
 
 
         app.post("/users", async (req, res) => {
@@ -40,10 +71,22 @@ async function run(){
         })
 
 
-        app.get('/all-todo', async(req, res)=>{
+        app.get('/all-todo', verifyJWT, async(req, res)=>{
           const email = req.query.email;
+          const decoded = req.decoded;
+          if(decoded.email !== email){
+          return  res.status(403).send({message: "unathorized access"})
+          }
           const query = { email: email };
           const result = await todosCollection.find(query).sort({ $natural: -1 }).toArray();
+          res.send(result);
+        })
+
+        app.get('/search', async(req, res)=>{
+          const search = req.query.search;
+          const query = { }
+          const allTodo = await todosCollection.find(query).toArray()
+          const result = allTodo.filter(todo=> todo?.title?.includes(search) || todo?.description?.includes(search)  )
           res.send(result);
         })
 
@@ -60,6 +103,7 @@ async function run(){
           const result = await deletedCollection.find(query).sort({ $natural: -1 }).toArray();
           res.send(result)
         })
+
 
         app.patch("/edit", async(req, res)=>{
           const {id,description} = req.body;
@@ -98,7 +142,6 @@ async function run(){
 
         app.delete("/delete/:id", async(req, res)=>{
           const id = req.params.id;
-          console.log(id)
           const query = { _id: new ObjectId(id) };
           const todo = await todosCollection.findOne(query);
           const deleteCollection = await deletedCollection.insertOne(todo);
